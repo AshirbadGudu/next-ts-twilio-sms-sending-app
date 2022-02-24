005635bbff274f027d33b248df6ea1ff8ffcd539
Ashirbad Panigrahi <ashirbadapanigrahi@gmail.com>
2022-02-24 18:08:19 +0530

    SMS sending logic with twilio added

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,29 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import twilio from 'twilio'
 
 type Data = {
-  name: string
+  message: any
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const accountSid = process.env.TWILIO_ACCOUNT_SID
+  const authToken = process.env.TWILIO_AUTH_TOKEN
+  const client = twilio(accountSid, authToken)
+  client.messages
+    .create({
+      body: 'Hello From Twilio',
+      from: '+19035827937',
+      to: '+917008614546',
+    })
+    .then((message) => {
+      console.log(message)
+      res.status(200).json({ message: 'Message sent successfully' })
+    })
+    .catch((err) => {
+      console.log(err)
+      res.status(500).json({ message: 'Message sending failed' })
+    })
 }
